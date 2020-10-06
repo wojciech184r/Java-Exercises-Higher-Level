e042696c8bdaeca4634ec6603f84d7f75de25af1
wojciech184r <wrajczakowski@yahoo.co.uk>
2020-10-06 19:42:57 +0100

    Exercise_3

--- src/com/Exercise3/Exercise3.java
+++ src/com/Exercise3/Exercise3.java
@@ -4,6 +4,21 @@
 
 package com.Exercise3;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class Exercise3 {
 
+    public void zbior(Map<String,Integer>name) {
+        Iterator iterator = name.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry entry = (Map.Entry) iterator.next(); // przesuwa iterator
+            //jeżeli jest następny element to endingString = ,
+            //jeżeli nie ma następnego elementu to endingString = .
+            String endingString = iterator.hasNext() ? "," : ".";
+            System.out.println("Key " + entry.getKey() + " : " + " Value " + entry.getValue() +endingString);
+        }
+    }
 }
--- src/com/Exercise3/Main.java
+++ src/com/Exercise3/Main.java
@@ -1,5 +1,26 @@
 package com.Exercise3;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        Exercise3 exercise3 = new Exercise3();
+
+        Map<String,Integer>zbior =new TreeMap<>();
+
+        zbior.put("Jan",123);
+        zbior.put("Krzys",432);
+        zbior.put("Ania",341);
+        zbior.put("Bsia",987);
+        zbior.put("Zbyszek",532);
+
+        System.out.println("number of name: "+zbior.size());
+
+        exercise3.zbior(zbior);
+    }
 
 }
