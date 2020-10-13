1a55ae61726e2ebd5610147c52d3fc38aac3e2d4
wojciech184r <wrajczakowski@yahoo.co.uk>
2020-10-13 21:52:50 +0100

    Exercise-10

--- src/com/Exercise10/Main.java
+++ src/com/Exercise10/Main.java
@@ -5,23 +5,23 @@ import static com.Exercise10.EngyneType.V8;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("VW","Golf",20000,2020,V12);
-        Car car2=new Car("Merc","SLS",200000,2020,V8);
-        Manufacturer manufacturer = new Manufacturer("Audi",1945,"Germany");
+        Car car = new Car("VW", "Golf", 20000, 2020, V12);
+        Car car2 = new Car("Merc", "SLS", 200000, 2020, V8);
+        Manufacturer manufacturer = new Manufacturer("Audi", 1945, "Germany");
 
         System.out.println(manufacturer);
         System.out.println(car);
         System.out.println(car2);
-<<<<<<< HEAD
-        System.out.println("Is Object 1 and Object 2 are the same = "+car.equals(car2));
+
+        System.out.println("Is Object 1 and Object 2 are the same = " + car.equals(car2));
         System.out.println(car.hashCode());
         System.out.println(car2.hashCode());
 
-=======
+
         System.out.println(car.equals(car2));
         System.out.println(car.hashCode());
         System.out.println(car2.hashCode());
-        
->>>>>>> 3c3d4a3b42c573727d42baa160fd32a0a3456759
+
+
     }
 }
