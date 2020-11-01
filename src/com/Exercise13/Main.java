d71b8ee25aae34737ec5430a3d8ee9317af5eb9f
wojciech184r <wrajczakowski@yahoo.co.uk>
2020-11-01 22:42:24 +0000

    Exercise13

---
+++ src/com/Exercise13/Main.java
@@ -0,0 +1,10 @@
+package com.Exercise13;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car.FIAT.isFasterThan(Car.FERRARI);
+        Car.isRegular(Car.FERRARI);
+        Car.isPremium(Car.FERRARI);
+    }
+}
